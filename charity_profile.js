501fdaa614d3479b0a95c2d4ed17e6eee162f689
AllisonGatt <116971163+AllisonGatt@users.noreply.github.com>
2024-12-03 15:14:50 -0500

    updates

--- charity_profile.js
+++ charity_profile.js
@@ -100,7 +100,7 @@ function listView() {
 // Grid View
 function gridView() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
+    elements[i].style.width = "35%";
   }
 }
 
