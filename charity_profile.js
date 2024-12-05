8cb7f178f12f49a53d105a97f8d787812046aadd
AllisonGatt <116971163+AllisonGatt@users.noreply.github.com>
2024-12-04 22:04:41 -0500

    styles

--- charity_profile.js
+++ charity_profile.js
@@ -87,16 +87,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //additional script for list/grid view on charitieslist.html
 var elements = document.getElementsByClassName("column");
-
 var i;
-
 // List View
 function listView() {
   for (i = 0; i < elements.length; i++) {
     elements[i].style.width = "100%";
   }
 }
-
 // Grid View
 function gridView() {
   for (i = 0; i < elements.length; i++) {
